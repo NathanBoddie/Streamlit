c4bbb6763d42b1bed3c177cd1a84ed387dd5e8fb
NathanBoddie <137906221+NathanBoddie@users.noreply.github.com>
2025-11-28 20:36:42 -0500

    Refactor NE111Project.py to restore imports and functions

--- NE111Project.py
+++ NE111Project.py
@@ -5,8 +5,7 @@ from typing import Tuple, Dict, Any, List
 
 import numpy as np
 import pandas as pd
-scipy==1.7.0
-import scipy.stats
+import scipy.stats == 1.7.0
 import matplotlib.pyplot as plt
 import streamlit as st
 
@@ -361,6 +360,7 @@ with tabs[2]:
 
             st.subheader("Parameter values")
             st.dataframe(pretty_params_table(params), height=200)
+
 
 
 
